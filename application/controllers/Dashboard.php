a60757e85b0085813cc589041bdb6121052a225c
riskaaprilia <riska.aprilia022@gmail.com>
2019-10-11 14:27:37 +0700

    tampilan

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public function index()
+	{
+        $this->header();
+        $this->load->view('v_dashboard');
+        $this->footer();
+    }
+    public function header()
+	{
+        
+        $this->load->view('v_header');
+       
+    }
+    public function footer()
+	{
+        
+        $this->load->view('v_footer');
+       
+    }
+}
+
